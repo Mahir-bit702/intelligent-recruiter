170e068897a17a362aa745378f65a8b14a6c718b
Mahir-bit702 <mahirf739@gmail.com>
2026-05-24 02:52:31 +0800

    Add Vite project structure for Vercel deployment

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import IntelligentRecruiter from './IntelligentRecruiter'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <IntelligentRecruiter />
+  </React.StrictMode>
+)
